9de0738ee297837d2e51a06f257343b7906fce5d
lengyxin <lengyingxin8966@gmail.com>
2018-12-23 14:41:40 +0800

    update

---
+++ src/pages/user_address/index.tsx
@@ -0,0 +1,43 @@
+import { View } from '@tarojs/components';
+import { observer } from '@tarojs/mobx';
+import Taro, { Component, Config } from '@tarojs/taro';
+import './index.less';
+@observer
+class Index extends Component {
+
+  /**
+   * 指定config的类型声明为: Taro.Config
+   *
+   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+   */
+  config: Config = {
+    navigationBarTitleText: '千里首铺',
+    // 下拉刷新
+    // enablePullDownRefresh: true,
+    // backgroundTextStyle: "dark"
+  }
+  componentWillMount() {
+  }
+  componentWillReact() {
+  }
+
+  componentDidMount() { }
+
+  componentWillUnmount() { }
+
+  componentDidShow() { }
+
+  componentDidHide() { }
+  onSearchBar() { }
+  render() {
+    return (
+      <View className='index'>
+        详情
+      </View>
+    )
+  }
+}
+
+export default Index
